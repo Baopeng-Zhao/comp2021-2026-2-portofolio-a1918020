63c70b9a09165868559c07733c1b2e7a51b044cd
Baopeng-Zhao <zhaobaopeng61@gmail.com>
2026-08-13 16:18:52 +0930

    Task3

---
+++ Topic1/PracB/Person.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿public class UnitTest1
+{
+    [Fact]
+    public void FullName_ReturnsExpectedFormat()
+    {
+        Person person = new Person("Ian", "Zhao", 21);
+
+        string result = person.FullName();
+
+        Assert.Equal("Zhao Ian", result);
+    }
+
+    [Fact]
+    public void IsAdult_ReturnsTrue_WhenAge18OrMore()
+    {
+        Person person = new Person("Ian", "Wang", 21);
+
+        bool result = person.IsAdult();
+
+        Assert.True(result);
+    }
+}
