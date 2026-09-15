c5a80d81157bcffa801f9d2ee61c8fc6d8cdc516
Baopeng-Zhao <zhaobaopeng61@gmail.com>
2026-09-15 12:01:50 +0930

    feat: Task1 and Reflection

---
+++ Topic7/PracA/LeaderBoard/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO.Compression;
+
+void DisplayScores(SortedSet<int> scores)
+{
+    int lowest = scores.ElementAt(1);
+    int highest = scores.ElementAt(1);
+    foreach (int score in scores)
+    {
+        Console.WriteLine(score);
+        if(score > highest)
+        {
+            highest = score;
+        }
+
+        if(score < lowest)
+        {
+            lowest = score;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"The highest score is: {highest}");
+    Console.WriteLine($"The lowest score is: {lowest}");
+}
+
+SortedSet<int> scores = new SortedSet<int>{2, 5, 1, 3, 4, 6, 8, 7, 9, 10};
+DisplayScores(scores);
+
+scores.Add(0);
+scores.Add(11);
+DisplayScores(scores);
+
+scores.Add(1);
+DisplayScores(scores);
